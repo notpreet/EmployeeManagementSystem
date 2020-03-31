8f4663a04f6ca73c14757a63bc3dff5edfc0ed4b
notpreet <preet51199@gmail.com>
2020-03-31 15:17:07 +0530

    with login backend

--- index.php
+++ index.php
@@ -3,10 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-signin-client_id" content="cybernetic-tide-272617">
     <title>Login</title>
     <link rel="stylesheet" type="text/css" href="assets/index.css" />
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
 </head>
 <body>
     <div id="logincontainer">
