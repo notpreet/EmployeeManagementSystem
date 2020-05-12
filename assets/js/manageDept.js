514e05884b50e2757f28985e407e4609a73c13c9
notpreet <preet51199@gmail.com>
2020-05-12 12:32:17 +0530

    added js

---
+++ assets/js/manageDept.js
@@ -0,0 +1,11 @@
+function unhide() {
+    document.querySelector(".addnew").style.display = "none";
+    document.querySelector(".hoide").style.display = "block";
+
+}
+
+function unhide2() {
+    document.querySelector(".hoide").style.display = "none";
+    document.querySelector(".addnew").style.display = "block";
+
+}
